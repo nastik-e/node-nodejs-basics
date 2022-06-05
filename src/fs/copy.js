6917a68100557e0f24d953746b0df52c98d5d7f7
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 23:08:02 +0300

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,8 @@
 export const copy = async () => {
     // Write your code here 
+    const fs = require('fs');
+    fs.copyFile('files','files_copy', function (error) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    })
 };
