4bbc0d9f30e2a4aa6779ba8c021d0f25a549b4fd
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 23:24:14 +0300

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,9 @@
 export const list = async () => {
     // Write your code here 
+        var fs = require('fs');
+    console.log (fs.writeFile('/files/', function (error) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    }
+       })) 
 };
