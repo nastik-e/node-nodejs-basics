d6e54a5eca288f80b2ca87188e5cb3167f87ecdc
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 23:12:22 +0300

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,8 @@
 export const rename = async () => {
     // Write your code here 
+    var fs = require(‘fs’);
+    fs.rename('wrongFilename.txt', 'properFilename.md', function (error) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    })
 };
