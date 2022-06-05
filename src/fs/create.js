6b82348d5216e7e719f02d55968bdd5168b61a9e
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 22:57:32 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,10 @@
 export const create = async () => {
-    // Write your code here 
+    // Write your code here
+    var fs = require('fs');
+    fs.writeFile('/files/fresh.txt', "I am fresh and young" , function (error) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    }
+       });
+ 
 };
