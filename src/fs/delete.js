80a9994ef0a5a9f95dd0ea9dd35197f5a1654aac
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 23:14:29 +0300

    Update delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,9 @@
 export const remove = async () => {
     // Write your code here 
+        var fs = require(‘fs’);
+    
+    fs.unlink('fileToRemove.txt', function (error) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    })
 };
