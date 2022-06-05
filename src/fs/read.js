e2ab1a66be4bcafe6c71c9a33fbe97fddcd9073c
nastik-e <105751995+nastik-e@users.noreply.github.com>
2022-06-05 23:30:03 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,9 @@
 export const read = async () => {
-    // Write your code here 
+    // Write your code here
+        var fs = require('fs');
+    fs.readFile('fileToRead.txt', function (error, data) {
+    if (error) { console.log('FS operation failed');
+       return;        
+    } else console.log (data);
+       })
 };
